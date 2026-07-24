cae118c960e0fbc0efcf0ccebccc62353ba88abb
MathInfo1 <168642711+MathInfo1@users.noreply.github.com>
2026-07-24 11:25:25 +0200

    Added job data

--- fixed_data.py
+++ fixed_data.py
@@ -9,6 +9,24 @@ foodMenu = {
     6: [25, 15, 25]
 }
 
+jobOptions = {
+    1: ["Junior Engineer", False, 8, 0, 0],
+    2: ["Junior Engineer", True, 11, 0, 0],
+    3: ["Senior Engineer", True, 16, 0, 6400],
+    4: ["Junior Language Job", False, 8, 1, 0],
+    5: ["Junior Language Job", True, 10, 1, 0],
+    6: ["Senior Language Job", True, 15, 1, 3200],
+    7: ["Social Worker", False, 8, 2, 0],
+    8: ["Junior Therapist", True, 10, 2, 0],
+    9: ["Senior Therapist", True, 15, 2, 3200],
+    10: ["Athlete", False, 13, 3, 0],
+    11: ["Artist", False, 13, 3, 0],
+    12: ["Musician", True, 15, 3, 0],
+    13: ["Experienced Athlete", False, 14, 3, 6400],
+    14: ["Experienced Artist", False, 14, 3, 6400],
+    15: ["Experienced Musician", True, 16, 3, 6400]
+}
+
 def sleepLimit(age):
     if age < 1: return 12, 9
     if age < 3: return 10, 8.5
@@ -27,3 +45,11 @@ def printMenu():
     print("Menu 4: Home-meal, $10, 15, 15")
     print("Menu 5: Restaurant, $15, 10, 20")
     print("Menu 6: Restaurant, $25, 15, 25")
+
+def printJobOptions():
+    print("Job No. | Job name | High school diploma required? | pay per hour | field no. | hours experience required")
+    for jobName, jobDetails in jobOptions.items():
+        editedDetails = jobDetails.copy()
+        editedDetails[2] *= 2
+        editedDetails[4] //= 2
+        print(f"{jobName}: {"; ".join([str(x) for x in editedDetails])}")
