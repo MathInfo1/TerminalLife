306cc689b9b82f3849e0ed235d5f1543aef24d7d
MathInfo1 <168642711+MathInfo1@users.noreply.github.com>
2026-07-24 11:23:27 +0200

    Added apply, work, therapy commands

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def printProfile():
     print(f"{pI["name"]}, {pI["gender"]}, {pI["age"]} years old")
     print(f"Birthday: {pI["birthDay"]}/{pI["birthMonth"]}")
     print(f"(Family) Status: {pI["status"]}, Current Balance: {pI["balance"]}")
-    print(f"{"Currently no health problems! " if pI["problems"] == [""] else pI["problems"]}")
+    print(f"{"Currently no health problems! " if pI["problems"] else pI["problems"]}")
     print(f"Hunger: {pI["hunger"]}, Happiness: {pI["happiness"]}")
     print()
 
@@ -75,45 +75,76 @@ def askCommand():
                 if pI["school"][field] >= 30:
                     pI["school"][field] -= 30
                     print(f"You passed the exam with the grade: A!")
-                    if pI["school"][4] >= 8 * 16: pI["school"][5] += 4
+                    if pI["school"][4] >= 8 * 16: 
+                        pI["school"][5] += 4
+                        pI["school"][6 + field] += 4
                 elif pI["school"][field] >= 22:
                     pI["school"][field] -= 22
                     print(f"You passed the exam with the grade: B!")
-                    if pI["school"][4] >= 8 * 16: pI["school"][5] += 3
+                    if pI["school"][4] >= 8 * 16: 
+                        pI["school"][5] += 3
+                        pI["school"][6 + field] += 3
                 elif pI["school"][field] >= 15:
                     pI["school"][field] -= 15
                     print(f"You passed the exam with the grade: C")
-                    if pI["school"][4] >= 8 * 16: pI["school"][5] += 2
+                    if pI["school"][4] >= 8 * 16: 
+                        pI["school"][5] += 2
+                        pI["school"][6 + field] += 2
                 elif pI["school"][field] >= 9:
                     pI["school"][field] -= 9
                     print(f"You passed the exam with the grade: D")
-                    if pI["school"][4] >= 8 * 16: pI["school"][5] += 1
+                    if pI["school"][4] >= 8 * 16: 
+                        pI["school"][5] += 1
+                        pI["school"][6 + field] += 1
                 else:
                     pI["school"][field] = pI["school"][field] // 3
                     print(f"You failed the exam since you didn't learn enough. ")
                 pI["school"][4] += 1
                 if pI["school"][4] == 12 * 16: 
-                    if pI["school"][5] > 64: print(f"Congratulations, you got your high school diploma with {pI["school"][5]}/256 points! ")
+                    if pI["school"][5] > 64: 
+                        pI["job"][1] = True
+                        print(f"Congratulations, you got your high school diploma with {pI["school"][5]}/256 points! ")
                     else: print(f"You failed too many exams and couldn't get your high school diploma with {pI["school"][5]}/256 points. ")
             else: 
                 print("Exams can be started on weekdays between 9am and 2pm. They take 1.5h each. ")
                 askCommand()
                 return
-    elif command == "enroll":
-        pass
+    elif command.startswith("apply"):
+        place = command.removeprefix("apply").strip()
+        while not place in ["work"]:
+            place = input("Where do you want to apply (work): ").lower()
+        if place == "work":
+            printJobOptions()
+            job = ""
+            while not (job.isnumeric() and int(job) > 0 and int(job) <= 12):
+                job = input("Enter job number (1 - 12): ")
+            job = int(job)
+            if (jobOptions[job][1] and (not pI["job"][1] or pI["school"][6 + jobOptions[job][3]] < 25)) or (jobOptions[job][4] > pI["job"][3 + jobOptions[job][3]]):
+                print("Sorry, you do not meet the qualifications for the job. ")
+                return
+            else:
+                pI["job"][0] = job
+                pI["job"][2] = jobOptions[job][2]
+                print("Congratulations, you got the job! ")
+    elif command == "work":
+        if pI["job"][0] == 100: 
+            print("First apply for a job with 'apply'.")
+            askCommand()
+            return
+        pI["balance"] += pI["job"][2]
+        pI["job"][3 + jobOptions[pI["job"][0]][3]] += 1
+    elif command == "therapy":
+        pI["happiness"] = min(100, pI["happiness"] + randint(2, 7))
+        pI["health"] = min(100, pI["health"] + randint(1, 4))
     elif command == "hobby":
         pass
     elif command == "wish":
         pass
-    elif command == "work":
-        pass
-    elif command == "therapy":
-        pass
     elif command == "buy":
         pass
 
-
-    # TODO: ["enroll", "attend", "hobby", "wish", "work", "therapy", "buy"]
+    # TODO: ["attend", "hobby", "wish", "buy"]
+    # TODO: Add health (problems, effects, cure)
 
 def eat():
     if command == "eat menu":
@@ -131,22 +162,27 @@ def eat():
 printProfile()
 while True:
     startDay()
-    hour = 7 # Normal value: 0
+    hour = 0 # Normal value: 0
     timeSlept = 0
     pI["hunger"] -= 5 # Normal value: 20
     if pI["age"] < 18: pI["balance"] += pI["status"] * 6
     sleepLimit1, sleepLimit2 = sleepLimit(pI["age"])
-    while hour < 11: # Normal value: 24
+    while hour < 24: # Normal value: 24
         askCommand()
         hour += 0.5
 
     if timeSlept < sleepLimit2: 
         pI["health"] -= randint(1, randint(3, 5))
         pI["happiness"] -= randint(1, 5)
-        if random() < 0.02: pI["problems"].append("sleep deprived")
+        print("Critical levels of sleep, please improve quickly")
+        if random() < 0.01: 
+            pI["problems"].append("sleep deprived")
+            print("You are now sleep deprived. ")
+
     elif timeSlept < sleepLimit1:
         pI["health"] -= randint(0, randint(1, 3))
         pI["happiness"] -= randint(1, randint(2, 5))
+        print("You need to sleep longer everyday. ")
 
     nextDay()
     saveGame(filePath, pI, gI)
